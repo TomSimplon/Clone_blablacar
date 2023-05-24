1670f2e209c22c4258e69a6003979b3b35262edd
tom <tom.pustel.simplon@gmail.com>
2023-05-24 13:58:04 +0200

    initialisation du projet

---
+++ src/Controller/RideController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RideController extends AbstractController
+{
+    #[Route('/', name: 'app_ride')]
+    public function index(): Response
+    {
+        return $this->render('ride/homepage.html.twig', [
+            'controller_name' => 'RideController',
+        ]);
+    }
+}
