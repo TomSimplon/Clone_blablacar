2cf1b5fa4a25e2917b84b08981c35f1693791649
tom <tom.pustel.simplon@gmail.com>
2023-05-24 15:06:10 +0200

    initialisation de la base de données

---
+++ src/Entity/Ride.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RideRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RideRepository::class)]
+class Ride
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $departure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $destination = null;
+
+    #[ORM\Column]
+    private ?int $seats = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $ceated = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDeparture(): ?string
+    {
+        return $this->departure;
+    }
+
+    public function setDeparture(string $departure): self
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getSeats(): ?int
+    {
+        return $this->seats;
+    }
+
+    public function setSeats(int $seats): self
+    {
+        $this->seats = $seats;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCeated(): ?\DateTimeInterface
+    {
+        return $this->ceated;
+    }
+
+    public function setCeated(\DateTimeInterface $ceated): self
+    {
+        $this->ceated = $ceated;
+
+        return $this;
+    }
+}
